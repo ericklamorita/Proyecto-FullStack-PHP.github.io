45d28fbb870ac2e1700064fdc62d6479d64f4e41
ErickMora <erick20herrera09@gmail.com>
2026-09-08 16:00:29 -0600

    actualización en index.php
    Estructura inicial

--- index.php
+++ index.php
@@ -1,2 +1,63 @@
-<?php
-echo "XAMPP y PHP están funcionando correctamente";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Don Erick</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Tienda Don Erick</h1>
+        <p>Productos de calidad para todos</p>
+        <nav>
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#productos">Productos</a></li>
+                <li><a href="#retiro">Retiro en tienda</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="inicio">
+            <h2>Bienvenidos a la tienda Don Erick</h2>
+            <p>Tienda Fisica y virtual donde van a poder realizar sus compras</p>
+            
+        </section>
+        <section id="productos">
+            <h2>Productos</h2>
+
+            <article id="cargador-telefono">
+                <h3>Cargador de teléfono</h3>
+                <a href= "https://extremetechcr.com/producto/cargador-de-pared-unno-tekno-10a-pw5059wt/"> Ver cargador de telefono</a>
+                <p>Precio: ₡1.900</p>
+            </article>
+            <article id="cargador-carro">
+                <h3>Cargador para carro</h3>
+                <a href= "https://extremetechcr.com/producto/cargador-para-carro-imexx-dual-2-1-amp-ime-41247/"> Ver cargador para carro</a>
+                <p>Precio: ₡1.000</p>
+            </article>
+            <article id="bateria-portatil">
+                <h3>Bateria Portatil</h3>
+                <a href= "https://extremetechcr.com/producto/cargador-unno-tekno-portatil-power-pro-super-fast-dual-port-20w-negro-pw5067bk/"> Ver bateria portatil</a>
+                <p>Precio: ₡3.000</p>
+            </article>
+        </section>
+
+
+        <section id="retiro">
+            <h2>Retiro en tienda</h2>
+            <p>Realiza tu retiro en nuestra tienda física, con metodo en efectivo o tarjeta</p>
+        </section>
+    </main>
+
+    <footer id="contacto">
+        <p>Contacto: TiendaMia@gmail.com</p>
+    </footer>
+
+</body>
+
+</html>
